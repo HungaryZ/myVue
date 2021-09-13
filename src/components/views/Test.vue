342fe0a3c0db29dae2f88938de92599af66f3432
HungaryZ <546857083@qq.com>
2021-09-13 13:51:15 +0800

    test数字加减0913

---
+++ src/components/views/Test.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <button id="byn_add" @click="add_num()">+</button>
+            <h2 id="num">{{num}}</h2>
+        <button id="btn_del" @click="del_num()">-</button>
+    </div>
+</template>
+<script>
+export default {
+  name: 'test',
+  data: function () {
+    return {
+      num: 0
+    }
+  },
+  methods: {
+    add_num: function () {
+      console.log(123)
+      this.num++
+    },
+    del_num: function () {
+      this.num--
+    }
+  }
+}
+</script>
